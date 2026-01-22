5acd8860d6843580c7b9552df06268175123e1bf
javierlnmn <jlm03123@gmail.com>
2026-01-22 12:24:40 +0100

    Add bookmarks with form using frame

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -3,4 +3,24 @@ class BookmarksController < ApplicationController
     @bookmarks = Bookmark.where(folder_id: nil)
     puts @bookmarks
   end
+
+  def new
+    @bookmark = Bookmark.new
+    render "_form"
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params())
+    if @bookmark.save
+      redirect_to bookmarks_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def bookmark_params
+      params.expect(bookmark: [ :name, :url ])
+    end
 end
