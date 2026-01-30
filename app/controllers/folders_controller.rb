c5349ad584d5c63b826e5160dae92831c25b48c6
javierlnmn <jlm03123@gmail.com>
2026-01-30 14:55:34 +0100

    Create and update folder views and controller

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -1,14 +1,53 @@
 class FoldersController < ApplicationController
+  before_action :set_folder, only: %i[ edit update destroy ]
+
   def index
     @folders = Folder.all
     @bookmarks = Bookmark.where folder_id: nil
   end
+
   def new
+    @folder = Folder.new
+    render :new, layout: "modal"
   end
+
   def create
+    @folder = Folder.new folder_params
+
+    if @folder.save()
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to folders_path }
+      end
+    else
+      render :new, status: :unprocessable_entity, layout: "modal"
+    end
   end
+
   def edit
+    render :edit, layout: "modal"
   end
+
   def update
+    if @folder.update(folder_params)
+      redirect_to @folder.parent || folders_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
+
+  def destroy
+    parent = @folder.parent
+    @folder.destroy
+    redirect_to parent || folders_path
+  end
+
+  private
+    def set_folder
+      @folder = Folder.find(params[:id])
+    end
+
+    def folder_params
+      params.expect(folder: [ :name, :url ])
+    end
 end
