8217bc7f364e43694de0aaebb05860552cc7f797
javierlnmn <jlm03123@gmail.com>
2026-01-30 13:32:24 +0100

    Add bookmarks relationship in folder model

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,4 +1,7 @@
 class Folder < ApplicationRecord
   has_many :children, class_name: "Folder", foreign_key: "parent_id"
   belongs_to :parent, class_name: "Folder", optional: true
+  has_many :bookmarks, dependent: :destroy
+
+  validates :name, presence: true
 end
