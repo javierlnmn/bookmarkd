b595efe462c62f3b9532d276c09237566eabf57d
javierlnmn <jlm03123@gmail.com>
2026-01-31 19:31:36 +0100

    Generate authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_19_095024) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_31_175822) do
   create_table "bookmarks", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "description"
@@ -30,6 +30,24 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_19_095024) do
     t.index ["parent_id"], name: "index_folders_on_parent_id"
   end
 
+  create_table "sessions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "ip_address"
+    t.datetime "updated_at", null: false
+    t.string "user_agent"
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email_address", null: false
+    t.string "password_digest", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+  end
+
   add_foreign_key "bookmarks", "folders"
   add_foreign_key "folders", "folders", column: "parent_id"
+  add_foreign_key "sessions", "users"
 end
