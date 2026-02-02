911236c51d4928218a2b5530dde83483963864d7
javierlnmn <jlm03123@gmail.com>
2026-02-02 23:53:23 +0100

    Create tags table

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :sessions, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
   has_many :folders, dependent: :destroy
+  has_many :tags, dependent: :destroy
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_02_162359) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_02_225151) do
   create_table "bookmarks", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "description"
@@ -44,6 +44,15 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_02_162359) do
     t.index ["user_id"], name: "index_sessions_on_user_id"
   end
 
+  create_table "tags", force: :cascade do |t|
+    t.string "color"
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_tags_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email_address", null: false
@@ -57,4 +66,5 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_02_162359) do
   add_foreign_key "folders", "folders", column: "parent_id"
   add_foreign_key "folders", "users"
   add_foreign_key "sessions", "users"
+  add_foreign_key "tags", "users"
 end
