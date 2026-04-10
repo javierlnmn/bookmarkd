aacffff4c0356e1d79326ed3a97c99c9f9ff7975
javierlnmn <jlm03123@gmail.com>
2026-04-10 18:22:32 +0200

    Schema version update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_18_111307) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_10_162144) do
   create_table "bookmarks", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "description"
